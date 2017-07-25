d2e298e6828f1f2f3661f4d188e8bb251dadfea8
Nguyen Trung <nguyentrung0904@gmail.com>
2017-07-25 22:56:24 +0700

    Change to MVVM

--- ContactsBackup/HomeViewModel.swift
+++ ContactsBackup/HomeViewModel.swift
@@ -11,13 +11,63 @@ import Contacts
 import ContactsUI
 
 class HomeViewModel {
+	
+	var fetchingContactsCallback: ((Int) -> Void)?
+	var exportingContactsCallback: ((URL) -> Void)?
+
+	var contacts = [CNContact]()
+
 	init() {
 		
 	}
 	
-	func loadAddressBook() -> Void {
+	func fetchContactsList(){
+		let keysToFech = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactPhoneNumbersKey]
+
+		
+		let store = CNContactStore()
+		do {
+
+			let request = CNContactFetchRequest(keysToFetch: keysToFech as [CNKeyDescriptor])
+			try store.enumerateContacts(with: request, usingBlock: { (contact, stop) in
+				self.contacts.append(contact)
+			})
+			fetchingContactsCallback!(self.contacts.count)
+			//self.quantity.text = "\(String(describing: self.contacts!.count)) contacts"
+			self.contacts.forEach { (contact) in
+				print(contact.phoneNumbers.count)
+			}
+			print(self.contacts.count)
+			
+		} catch is NSError {
+			print("Cant fetch contacts")
+		}
 		
 	}
 	
+	
+	
+	func exportToCSV(){
+
+		let filename = "contacts.csv"
+		let path = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(filename)
+		var csvText="Name,Given Name,Family Name,Phone 1 - Type,Phone 1 - Value"
+		contacts.forEach { (contact) in
+			//print(contact.phoneNumbers[0].value)
+			
+			if let number = contact.phoneNumbers.first?.value.stringValue {
+				print(number)
+				csvText.append("\n\(contact.givenName),\(contact.givenName),\(contact.familyName),Mobile,\(number)")
+			}
+		}
+		do {
+			try csvText.write(to: path!, atomically: true, encoding: String.Encoding.utf8)
+			exportingContactsCallback!(path!)
+		} catch {
+			print("Failed to create file")
+			print("\(error)")
+		}
+		
+	}
 	
 }
--- ContactsBackup/ViewController.swift
+++ ContactsBackup/ViewController.swift
@@ -13,10 +13,11 @@ import ContactsUI
 class ViewController: UIViewController {
 
 	@IBOutlet weak var quantity: UILabel!
-	private var viewModel: HomeViewModel?
-	var contacts: [CNContact]?
+	private var viewModel = HomeViewModel()
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		viewModel.fetchingContactsCallback = updateLabelFetchingCallback
+		viewModel.exportingContactsCallback = exportToCSVCallback
 		
 	}
 	
@@ -31,63 +32,24 @@ class ViewController: UIViewController {
 
 	
 	@IBAction func loadClicked(){
-		fetch()
+		viewModel.fetchContactsList()
 	}
 	
 	
-	
-	func fetch(){
-		let keysToFech = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactPhoneNumbersKey]
-		
-		
-		let store = CNContactStore()
-		do {
-			//self.contacts = try store.unifiedContacts(matching: CNContact.predicate, keysToFetch: keysToFech as [CNKeyDescriptor])
-			let request = CNContactFetchRequest(keysToFetch: keysToFech as [CNKeyDescriptor])
-			contacts = [CNContact]()
-			try store.enumerateContacts(with: request, usingBlock: { (contact, stop) in
-				self.contacts?.append(contact)
-			})
-			self.quantity.text = "\(String(describing: self.contacts!.count)) contacts"
-			self.contacts!.forEach { (contact) in
-				print(contact.phoneNumbers.count)
-			}
-			print(self.contacts!.count)
-			
-		} catch is NSError {
-			print("Cant fetch contacts")
-		}
+	func updateLabelFetchingCallback(numberOfContact: Int){
+		self.quantity.text = "\(String(describing: numberOfContact)) contacts"
 
 	}
+
 	
 	
 	@IBAction func exportBtnClicked(){
-		guard (self.contacts != nil) else {
-			return;
-		}
-		exportToCSV(contacts: self.contacts!)
+
+		viewModel.exportToCSV()
 	}
 	
-	func exportToCSV(contacts: [CNContact]){
-		let filename = "contacts.csv"
-		let path = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(filename)
-		var csvText="Name,Given Name,Family Name,Phone 1 - Type,Phone 1 - Value"
-		contacts.forEach { (contact) in
-			//print(contact.phoneNumbers[0].value)
-			
-			if let number = contact.phoneNumbers.first?.value.stringValue {
-				print(number)
-				csvText.append("\n\(contact.givenName),\(contact.givenName),\(contact.familyName),Mobile,\(number)")
-			}
-		}
-		do {
-			try csvText.write(to: path!, atomically: true, encoding: String.Encoding.utf8)
-		} catch {
-			print("Failed to create file")
-			print("\(error)")
-		}
-		
-		let vc = UIActivityViewController(activityItems: [path!], applicationActivities: [])
+	func exportToCSVCallback(path: URL){
+		let vc = UIActivityViewController(activityItems: [path], applicationActivities: [])
 		present(vc, animated: true, completion: nil)
 	}
 	
